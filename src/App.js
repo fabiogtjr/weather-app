127e98c36b854a6d750603ef211494dc96910cb3
Fábio Garcia <fabio.garcia@quaestum.com.br>
2022-03-13 17:26:14 -0300

    Weather API functional

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getWeather } from "./actions/weather";
 
 const App = () => {
+  const [weather, setWeather] = useState({});
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      ({ coords: { latitude, longitude } }) =>
+        handleWeather(latitude, longitude)
+    );
+  }, []);
+
+  const handleWeather = async (latitude, longitude) => {
+    const data = await getWeather(latitude, longitude);
+    console.log(data);
+    setWeather(data);
+  };
+
   return (
     <div>
       <header>Olá Mundo!</header>
---
+++ src/actions/weather.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+export const getWeather = async (lat, long) => {
+  if (!lat || !long) return [];
+  try {
+    const { data, status } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&lang=pt_br&appid=096919e1d26c1b30ba3b4d14693aed3f`
+    );
+    if (status === 200) {
+      return {
+        ...data?.main,
+        ...data?.weather[0],
+        ...data?.wind,
+        cityName: data?.name,
+        country: data?.sys?.country,
+      };
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
