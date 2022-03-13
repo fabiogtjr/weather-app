297848896974d05c035ca22f7c37031a17988ea5
Fábio Garcia <fabio.garcia@quaestum.com.br>
2022-03-13 17:38:45 -0300

    Add View

--- src/App.js
+++ src/App.js
@@ -18,7 +18,13 @@ const App = () => {
 
   return (
     <div>
-      <header>Olá Mundo!</header>
+      <header>
+        Olá, está {Math.round(weather?.temp)}º em {weather?.cityName} -{" "}
+        {weather?.country}, sensação termica de{" "}
+        {Math.round(weather?.feels_like)}º, com previsão de maxima entorno de{" "}
+        {Math.round(weather?.temp_max)}º e minima{" "}
+        {Math.round(weather?.temp_min)}º, com previsão de {weather?.description}
+      </header>
     </div>
   );
 };
